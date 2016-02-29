4f2ce389c42dd8230fc587743040ec8a1480c2b3
Nazaret <samaht15@gmail.com>
2016-02-28 23:48:46 -0430

    imagenes de logos - resize, font family - 70%, sombra más oscuras, sin gradiant

--- javascript.js
+++ javascript.js
@@ -1,18 +1,37 @@
 $(document).ready(function(){
-
-	window.sr = ScrollReveal({ reset: true });
-	sr.reveal( '.social, .us', { 
-		duration: 2000,
+	$(window).scrollTop(0);
+	var spreveal={
+		duration: 200,
 		distance    : '10000px',
 		origin      : 'bottom',
-		viewFactor  : 1.0,
-		container   : document.querySelector('.other-div')	
-	});
-	/*$(".social").hide();
+		viewFactor  : 0.2
+	};
+	
+/*
+	jQuery('.social, .us').addClass("fixed");
+	$(".social").hide();
 	$(".us").hide();
 	$(window).scroll(function(){
 		if (document.getElementsByClassName("intro"))
         $(".social").show();
         $(".us").show();
 	});*/
+	
+	var win = $(window);
+	  // definir mediente $pos la altura en píxeles desde el borde superior de la ventana del navegador y el elemento
+	var pos = 800;
+	window.sr = ScrollReveal();
+	win.scroll(function () {
+	    if (win.scrollTop() >= pos){
+	     	$('.social, .us').removeClass('hidden');
+	     	$('.social, .us').addClass('show');
+	     	
+			sr.reveal( '.social, .us', spreveal);
+	    }
+	    else {
+      		$('.social, .us').removeClass('show');
+      		$('.social, .us').addClass('hidden');
+     	}
+   	});
+	
 });
